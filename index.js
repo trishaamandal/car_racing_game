d52b5a229beaba31f1b92a74b0d9b633fb3df2e7
trisha mandal <154074997+trishaamandal@users.noreply.github.com>
2024-04-18 10:55:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const highScore = document.querySelector('.highScore');
 const startScreen = document.querySelector('.startScreen');
 const gameArea = document.querySelector('.gameArea');
 const ClickToStart = document.querySelector('.ClickToStart');
-// const grass = document.querySelector('.grass');
-// const garden = document.querySelector('.garden');
 ClickToStart.addEventListener('click', Start);
 document.addEventListener('keydown', keydown);
 document.addEventListener('keyup', keyup);
@@ -14,3 +12,38 @@ ArrowDown: false,
 ArrowLeft: false,
 ArrowRight: false,
 }
+let player = {
+    speed: 5,
+    score: 0,
+    highScore: 0
+};
+function keydown(e) {
+    keys[e.key] = true
+}
+function keyup(e) {
+    keys[e.key] = false;
+}
+function Start() {
+    gameArea.innerHTML = "";
+    startScreen.classList.add('hide');
+    player.isStart = true;
+    player.score = 0;
+    window.requestAnimationFrame(Play);
+}
+for (i = 0; i < 5; i++) {
+    let roadLines = document.createElement('div');
+    roadLines.setAttribute('class', 'roadLines');
+    roadLines.y = (i * 140);
+    roadLines.style.top = roadLines.y + "px";
+    gameArea.appendChild(roadLines);
+}
+for (i = 0; i < 3; i++) {
+    let Opponents = document.createElement('div');
+    Opponents.setAttribute('class', 'Opponents');
+    Opponents.y = ((i) * -300);
+    Opponents.style.top = Opponents.y + "px";
+    gameArea.appendChild(Opponents);
+    Opponents.style.left = Math.floor(Math.random() * 350) + "px";
+    Opponents.style.backgroundColor = randomColor();
+}
+
